59fd39bad9f48d6e0ae979ce1199415e63e75b44
Gilda M Castro Sancho <59642399+gmcastros@users.noreply.github.com>
2020-02-26 09:14:40 +0100

    adding steps 3,4

--- run_analysis.R
+++ run_analysis.R
@@ -35,6 +35,36 @@ subject_test_data <- read.table(subject_test)
 y_test_data <- read.table(y_test)
 x_test_data <- read.table(x_test)
 
+# This is the dataset with all data
 all_data <- rbind(cbind(subject_test_data, x_test_data, y_test_data)
                   ,cbind(subject_train_data, x_train_data, y_train_data))
 
+
+#Step 2. Extracts only the measurements on the mean and standard deviation for each measurement
+#Get the features data so we know what we are looking for. I need to collect the indexes of the mean and std
+features_data <- read.table(features)
+
+# Get the indexes where "mean" or "std" says. Use a regular expresion
+# line sample form file: fBodyGyro-mean()-Z
+mean_std_indexes <- grep("mean\\(\\)|std\\(\\)", features_data[[2]])
+
+# We need to add 2 columns to the indexes data table, otherwise we cannot merge it with the "all_data"
+mean_std_variables <- c(1,2,mean_std_indexes+2)
+
+#Get the data related to mean and std using the previously collected indexes
+filtered_data <- all_data[, mean_std_variables]
+
+#Step 3. Uses descriptive activity names to name the activities in the data set
+#sample:
+#  V1                 V2
+#1  1            WALKING
+
+activity_labels_data <- read.table(activity_labels)
+filtered_data[[2]] <- factor(filtered_data[[2]], levels = activity_labels_data[[1]], labels = activity_labels_data[[2]])
+
+#Step 4: Appropriately labels the data set with descriptive variable names.
+# Names are here
+descriptive_names <- features_data[[2]][mean_std_indexes]
+
+filtered_data_with_names <- filtered_data
+names(filtered_data_with_names) <- c("subject", "activity", descriptive_names)
